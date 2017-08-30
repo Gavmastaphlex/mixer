ecc9774b771ff811c5a107d23c2bab3bec71df3b
Gavin McGruddy <gavin.mcgruddy@tekura.school.nz>
2017-08-30 16:50:17 +1200

    Adjusted Nav bar, added icons, added more jQuery functionality and altered other styles.

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$('.hamburger').click(function() {
-		if($(this).hasClass( "is-active" )) {
+	$('#hamburger-container').click(function() {
+		$('#perspective').toggleClass("modalview animate"); 
+		if($('.hamburger').hasClass( "is-active" )) {
 			$('.hamburger').removeClass( "is-active" );
 			$('.container').trigger("click");
-			console.log('The container should be clicked...');
 		} else {
 			$('.hamburger').addClass( "is-active" );
 		}
@@ -15,65 +15,44 @@ $(document).ready(function() {
 
 	})
 
-	$('#logoutBtn.myButton').click(function(event) {
-		event.preventDefault();
-		$('#logoutButton').click();
+	$('.fa-sign-in').click(function(){
+		$(this).toggleClass("down"); 
+		if($('.login-box').css('display') != 'block') {
+			$('.login-box').slideDown('slow');
+			setTimeout(function(){
+			  $(".login-box input:first").focus();
+			}, 500);
+		} else {
+			$('.login-box').slideUp('slow');
+		}
+		
+	})
 
+	$('.fa-sign-out').click(function(){
+		$('#logoutButton').click();
 	})
 
 
-var search = 0;
-var searchRunning = 0;
-var scrollThreshold = 0;
+	$('.animate .container').click(function() {
+		$('.animate').removeClass('animate');
+		$('.modalview').removeClass('modalview');
+	})
 
-	$(window).scroll(function() {
-
-		 var navHeight =  120;
-
-		 $('.pulse-down').fadeOut('slow');
-	 
-	    clearTimeout($.data(this, 'scrollTimer'));
-	    $.data(this, 'scrollTimer', setTimeout(function() {
-	       
-			if ($(window).scrollTop() > navHeight && scrollThreshold == 0) {
-
-				$('#header').css({position:'fixed', top: '-114px', backgroundColor: 'rgba(106, 106, 106, 0.58)', height: '80px'});
-				$('#header').animate({top: '0px'}, 300);
-				$('#header h1').animate({width: '192px', height:'64px'}, 300);
-				$('#header h1 a').animate({fontSize: '70px'}, 300);
-
-				scrollThreshold = 1;
-
-			} else if ($(window).scrollTop() == 0 && scrollThreshold == 1) {
-
-				$('#header').css({backgroundColor: 'transparent', position:'absolute', top: '0px'});
-				$('#header h1').animate({ width: '292px', height:'124px'}, 300);
-				$('#header h1 a').animate({fontSize: '110px'}, 300);
-				$('#header').animate({height: '130px'}, 300);
-
-				scrollThreshold = 0;
-
-			} else if ($(window).scrollTop() < navHeight && scrollThreshold == 1) {
-
-				$('#header').css({backgroundColor: 'transparent'});
-
-				$( "#header" ).animate({
-				    top: '-114px',
-				    backgroundColor: 'transparent',
-				    fontSize : '13px',
-				    height: '80px'
-				  }, 300, function() {
-				    $('#header').css({position:'absolute', top: '0px', });
-				  });
-
-				$('#header h1').animate({width: '192px', height:'64px'}, 300);
-				$('#header h1 a').animate({fontSize: '70px'}, 300);
-
-				scrollThreshold = 0;
-
-			}
-	    }, 100));
+	$(document).click(function() {
+		if($('.login-box').css('display') == 'block') {
+			$('.login-box').slideUp('slow');
+			$('.fa-sign-in').toggleClass("down"); 
+		}
 	});
 
+	$(".fa-sign-in").click(function(e) {
+		e.stopPropagation(); // This is the preferred method.
+		return false;        // This should not be used unless you do not want
+		// any click events registering inside the div
+	});
+
+	$(".login-box").click(function(e) {
+		e.stopPropagation(); // This is the preferred method.
+	});
 
 });
--- www/views/viewClass.php
+++ www/views/viewClass.php
@@ -33,10 +33,6 @@ abstract class View {
     
     private function displayHeader() {
 
-        if($_GET['page'] == 'userPanel') {
-            
-        }
-
         /*
         Displaying all the header information
         */
@@ -45,6 +41,7 @@ abstract class View {
         $html .= '<head>'."\n";
         $html .= '<meta charset="UTF-8">'."\n";
         $html .= '<title>'.$this -> pageInfo['pageTitle'].'</title>'."\n";
+        $html .= '<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />'."\n";
         $html .= '<link rel="stylesheet" type="text/css" href="css/menu.css" />'."\n";
         $html .= '<link rel="stylesheet" type="text/css" href="css/styles.css" />'."\n";
         $html .= '<link href="http://fonts.googleapis.com/css?family=Text+Me+One|Raleway:400,200" rel="stylesheet" type="text/css" />'."\n";
@@ -62,7 +59,33 @@ abstract class View {
         $html .= '</span>'."\n";
         $html .= '</button>'."\n";
         $html .= '</div> <!-- eo#hamburger-container -->'."\n";
-        $html .= '<h1><a id="logo" href="index.php?page=home">Mixer</a></h1>'."\n";
+        $html .= '<h1 id="logo"><a href="index.php?page=home">Mixer</a></h1>'."\n";
+        
+
+        if($_SESSION['userName'] && !$_POST['deleteProfile']) {
+
+        $html .= '<a href="index.php?page=userPanel"><i class="fa fa-user" aria-hidden="true"></i></a>'."\n";
+        $html .= '<i class="fa fa-sign-out" aria-hidden="true"></i>'."\n";
+        $html .= '<form id="logoutForm" method="post" action="index.php">'."\n";
+        $html .= '<input type="submit" name="logoutButton" id="logoutButton" class="loginBtn" value="Logout" />'."\n";
+        $html .= '</form>'."\n";
+
+        } else {
+
+        $html .= '<i class="fa fa-sign-in" aria-hidden="true"></i>'."\n";
+        $html .= '<div class="login-box">'."\n";
+        $html .= '<h1>Login</h1>'."\n";
+        $html .= '<form method="post" action="index.php">';
+        $html .= '<input type="text" name="userName" placeholder="Username"/>'."\n";
+        $html .= '<input type="text" name="userPassword" placeholder="Password"/>'."\n";
+        $html .= '<input type="submit" name="loginButton" id="loginButton" class="loginBtn" value="Login" />'."\n";
+        $html .= '<button>Login</button>'."\n";
+        $html .= '</form>'."\n";
+        $html .= '<p>Not a member? <a href="index.php?page=register">Sign Up</a></p>'."\n";
+        $html .= '</div>'."\n";
+
+        }
+
         $html .= '</div>'."\n";
         $html .= '<div id="perspective" class="perspective effect-laydown">'."\n";
         $html .= '<div id="container" class="container">'."\n";
@@ -92,55 +115,13 @@ abstract class View {
         $html .= '<a href="index.php?page=contact" class="icon-image">Contact</a>'."\n";
         $html .= '<a href="index.php?page=about" class="icon-upload">About</a>'."\n";
 
+
         /*
         Checking to see if anyone is logged in or not so as to discern whether to display an empty
         login box, or a box that welcomes the user and gives them the option to either logout or
         to visit their user panel.
         */
-        if($_SESSION['userName'] && !$_POST['deleteProfile']) {
-
-        $html .= '<div id="loggedIn">'."\n";
-        $html .= '<p>Welcome '.$_SESSION['userName'].'</p>'."\n";
-        $html .= '<form method="post" action="index.php">'."\n";
-        $html .= '<a href="index.php?page=userPanel" class="myButton">User Panel</a>'."\n";
-        $html .= '<a href="#" class="myButton" id="logoutBtn">Logout</a>'."\n";
-        $html .= '<input type="submit" name="logoutButton" id="logoutButton" class="loginBtn" value="Logout" />'."\n";
-        $html .= '</form>'."\n";
-        $html .= '</div>'."\n";
-
-        } else {
-
-        $html .= '<!--div id="login">'."\n";
-        $html .= '<form method="post" action="index.php">';
-        $html .= '<div class="loginFields">'."\n";
-        $html .= '<label for="userName">Username:</label>'."\n";
-        $html .= '<input type="text" name="userName" id="userName" />'."\n";
-        $html .= '</div>'."\n";
-        $html .= '<div class="loginFields">'."\n";
-        $html .= '<label for="userPassword">Password:</label>'."\n";
-        $html .= '<input type="password" name="userPassword" id="userPassword" />'."\n";
-        $html .= '</div>'."\n";
-        $html .= '<div class="clearDiv"></div>'."\n";
-        $html .= '<a href="#" class="myButton" id="loginBtn">Login</a>'."\n";
-        $html .= '<a href="index.php?page=register" class="myButton">Register</a>'."\n";
-        $html .= '<input type="submit" name="loginButton" id="loginButton" class="loginBtn" value="Login" />'."\n";
-        $html .= '</form>'."\n";
-        $html .= '<p>'.$this -> model -> loginMsg.'</p>';
-        $html .= '<div class="clearDiv"></div>'."\n";
-        $html .= '</div-->'."\n";
-
-        $html .= '<div class="login-box">'."\n";
-        $html .= '<h1>Login</h1>'."\n";
-        $html .= '<form method="post" action="index.php">';
-        $html .= '<input type="text" name="userName" placeholder="Username"/>'."\n";
-        $html .= '<input type="text" name="userPassword" placeholder="Password"/>'."\n";
-        $html .= '<input type="submit" name="loginButton" id="loginButton" class="loginBtn" value="Login" />'."\n";
-        $html .= '<button>Login</button>'."\n";
-        $html .= '</form>'."\n";
-        $html .= '<p>Not a member? <a href="index.php?page=register">Sign Up</a></p>'."\n";
-        $html .= '</div>'."\n";
-
-        }
+       
 
         $html .= '</nav>'."\n";
         $html .= '</div>'."\n";
